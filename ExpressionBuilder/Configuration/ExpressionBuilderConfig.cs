3ba23587db9c74d2d91432938c3a081ee48c42ec
David Belmont <dbelmont@gmail.com>
2017-08-23 12:43:44 +1000

    • Adding a configSection to enable the configuration of the supported types. • Improving the *readme* file. • Adding a *readme* file to the NuGet package.

---
+++ ExpressionBuilder/Configuration/ExpressionBuilderConfig.cs
@@ -0,0 +1,55 @@
+﻿using ExpressionBuilder.Common;
+using System;
+using System.Configuration;
+
+namespace ExpressionBuilder.Configuration
+{
+    internal class ExpressionBuilderConfig : ConfigurationSection
+    {
+        public const string SectionName = "ExpressionBuilder";
+
+        private const string SupportedTypeCollectionName = "SupportedTypes";
+
+        [ConfigurationProperty(SupportedTypeCollectionName)]
+        
+        public SupportedTypesElementConfiguration SupportedTypes { get { return (SupportedTypesElementConfiguration)base[SupportedTypeCollectionName]; } }
+
+        public class SupportedTypeElement : ConfigurationElement
+        {
+            [ConfigurationProperty("type", IsRequired = true, IsKey = true)]
+            public string Type
+            {
+                get
+                {
+                    return (string)base["type"];
+                }
+            }
+
+            [ConfigurationProperty("typeGroup", IsRequired = true, IsKey = false)]
+            public TypeGroup TypeGroup
+            {
+                get
+                {
+                    return (TypeGroup)base["typeGroup"];
+                }
+            }
+        }
+
+        [ConfigurationCollection(typeof(SupportedTypesElementConfiguration), AddItemName = "add")]
+        public class SupportedTypesElementConfiguration : ConfigurationElementCollection
+        {
+            protected override ConfigurationElement CreateNewElement()
+            {
+                return new SupportedTypeElement();
+            }
+
+            protected override object GetElementKey(ConfigurationElement element)
+            {
+                if (element == null)
+                    throw new ArgumentNullException("element");
+
+                return ((SupportedTypeElement)element).Type;
+            }
+        }
+    }
+}
